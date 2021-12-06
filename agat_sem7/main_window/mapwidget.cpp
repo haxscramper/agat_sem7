da33eb3c09b173dcf87dfb4ff9615a3562cabba9
TyFeD <70333380+TyFeD@users.noreply.github.com>
2021-12-06 13:37:33 +0300

    Update mapwidget.cpp

--- agat_sem7/main_window/mapwidget.cpp
+++ agat_sem7/main_window/mapwidget.cpp
@@ -1,14 +1,41 @@
-#include "mapwidget.hpp"
-#include <QDebug>
+#ifndef MAPWIDGET_HPP
+#define MAPWIDGET_HPP
+
+#include "ship.h"
+
 #include <QGraphicsView>
+#include <QObject>
+#include <QGraphicsScene>
 
-MapWidget::MapWidget()
-    : view(new QGraphicsView())
-    , scene(new QGraphicsScene())
-//  ,
+class MapWidget : public QGraphicsView
 {
-    view->setScene(scene);
-}
+    Q_OBJECT
+  public:
+    MapWidget();
 
-QGraphicsView*  MapWidget::getView() const { return view; }
-QGraphicsScene* MapWidget::getScene() const { return scene; }
+    explicit MapWidget(int h, int w,QWidget *parent = nullptr);
+
+    void drawShip(int id, double x, double y,int course);
+
+    void drawEnemyShip(int id, double x, double y,int course);
+
+    void drawLine(int idLine, double x1, double y1, double x2, double y2, QPen pen);
+
+    void drawEllipse(int idElips, double x1, double y1, double x2, double y2, QPen pen);
+
+    QGraphicsView*  getView() const;
+    QGraphicsScene* getScene() const;
+
+  private:
+    QGraphicsView*  view  = nullptr;
+    QGraphicsScene* scene = nullptr;
+
+    QGraphicsScene scene_1;
+    Ship *ship;
+    Ship *enemy_ship;
+
+    int scale = 1; // масштаб карты
+    void SetupMetrix();
+};
+
+#endif // MAPWIDGET_HPP
